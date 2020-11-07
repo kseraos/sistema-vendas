3fb647600b08e21aa2a3d83495e4a604b596323f
kristine <kseraos@outlook.com>
2020-11-07 09:04:05 -0400

    seeder de usuario e redirect login

---
+++ database/seeds/UserSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+use App\User;
+use Illuminate\Database\Seeder;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        User::firstOrCreate(
+            [
+                'email' => 'admin@email.com'
+            ],
+            [
+                'name' => 'Administrador Padrão',
+                'password' => bcrypt('admin'),
+                'type' => '1'
+
+            ]
+            );
+    }
+}
